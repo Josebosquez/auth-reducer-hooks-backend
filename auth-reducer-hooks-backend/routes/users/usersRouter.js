b791d032da7fb41861932c333796299ff6075925
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-08-11 19:14:40 -0600

    login function is operational with postman, saves cookie and jwt

--- auth-reducer-hooks-backend/routes/users/usersRouter.js
+++ auth-reducer-hooks-backend/routes/users/usersRouter.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-const {createUser} = require('./controller/userController')
+const {createUser, login} = require('./controller/userController')
 
 router.get('/', function (req, res, next) {
     res.json({message: 'respond with a resource'})
 })
 
 router.post('/create-user', createUser)
+router.post('/login', login)
 
 module.exports = router
